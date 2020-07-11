2837b83881549a771c8ba61ecbee13e4a20e9b13
n0tva1id <172410245@qq.com>
2020-07-11 14:57:39 +0800

    Header Added

--- DNS_Lab/main.c
+++ DNS_Lab/main.c
@@ -1,5 +1,22 @@
 #include "getopt.h"
 
+
+struct header {
+	unsigned short ID;
+	unsigned char QR : 1;
+	unsigned char Opcode : 4;
+	unsigned char AA : 1;
+	unsigned char TC : 1;
+	unsigned char RD : 1;
+	unsigned char RA : 1;
+	unsigned char Z : 4;
+	unsigned char RCODE : 4;
+	unsigned short QDCOUNT;
+	unsigned short ANCOUNT;
+	unsigned short NSCOUNT;
+	unsigned short ARCOUNT;
+};
+
 int main(int argc, char *argv[])
 {
 	return 0;
